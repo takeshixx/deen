a6bea11bbc100713079df5340f5235d480491f8f
takeshix <takeshix@adversec.com>
2016-10-17 21:24:01 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='DeEn',
+      version='v0.1.0',
+      scripts=['deen.py'],
+      install_requires=['PyQt5'],
+      url='https://github.com/takeshixx/deen',
+      license='Apache 2.0',
+      author='takeshix',
+      description='Generic decoding/encoding application')
