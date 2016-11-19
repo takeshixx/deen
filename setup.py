2979bbfd765b635bd5899b42ae1e9fb5344c23e6
takeshix <takeshix@adversec.com>
2016-11-19 01:43:50 +0100

    Fixed version in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='DEEN',
-      version='v0.4.0',
+      version='0.4.0',
       install_requires=['PyQt5'],
       packages=['deen',
                 'deen.widgets'],
