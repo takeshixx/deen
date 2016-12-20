9ad515d233c265fafa752b8c09baf21af3f6f31e
takeshix <takeshix@adversec.com>
2016-12-20 17:10:32 +0100

    Added command line and stdin file read support

--- deen/main.py
+++ deen/main.py
@@ -1,6 +1,7 @@
 import sys
 import logging
 import os.path
+import argparse
 
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtGui import QIcon
@@ -11,10 +12,28 @@ ICON = os.path.dirname(os.path.abspath(__file__)) + '/icon.png'
 LOGGER = logging.getLogger()
 logging.basicConfig(format='[%(lineno)s - %(funcName)s() ] %(message)s')
 
+ARGS = argparse.ArgumentParser()
+ARGS.add_argument('infile', nargs='?', default=None)
+ARGS.add_argument('outfile', nargs='?', default=None)
+
 
 def main():
+    args = ARGS.parse_args()
+    content = None
+    if args.infile:
+        if args.infile == '-':
+            try:
+                stdin = sys.stdin.buffer
+            except AttributeError:
+                stdin = sys.stdin
+            content = stdin.read()
+        else:
+            with open(args.infile, 'rb') as f:
+                content = f.read()
     app = QApplication(sys.argv)
     ex = Deen()
+    if content:
+        ex.encoder_widget.set_root_content(content)
     ex.setWindowIcon(QIcon(ICON))
     LOGGER.addHandler(ex.log)
     return app.exec_()
