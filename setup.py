f5448383207bb26428771bbc6a5afe69b19ad55e
takeshix <takeshix@adversec.com>
2017-01-29 00:42:59 +0100

    Removed .desktop file

--- setup.py
+++ setup.py
@@ -14,8 +14,6 @@ setup(name='deen',
       url='https://github.com/takeshixx/deen',
       package_data={
           'deen': ['icon.png']},
-      data_files=[
-          ('share/applications', ('freedesktop/deen.desktop',))],
       license='Apache 2.0',
       author='takeshix',
       description='Generic decoding/encoding application')
