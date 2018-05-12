9c6852831779e8603827bba4c22a48b1a0a28dbb
takeshix <takeshix@adversec.com>
2018-05-13 00:51:54 +0200

    Added jsontoxml and xmltojson formatters

---
+++ deen/plugins/formatters/jsontoxml.py
@@ -0,0 +1,46 @@
+import json
+
+try:
+    import dicttoxml
+except ImportError:
+    DICTTOXML = False
+else:
+    DICTTOXML = True
+
+from .. import DeenPlugin
+
+
+class DeenPluginJsonToXmlFormatter(DeenPlugin):
+    name = 'jsontoxml_formatter'
+    display_name = 'JSON to XML (f)'
+    aliases = ['j2x',
+               'json2xml']
+    cmd_name = 'jsontoxml'
+    cmd_help = 'Convert JSON object to XML'
+
+    def __init__(self):
+        super(DeenPluginJsonToXmlFormatter, self).__init__()
+
+    @staticmethod
+    def prerequisites():
+        try:
+            import dicttoxml
+        except ImportError:
+            return False
+        else:
+            return True
+
+    def process(self, data):
+        super(DeenPluginJsonToXmlFormatter, self).process(data)
+        if not DICTTOXML:
+            return
+        try:
+            data = json.loads(data.decode())
+        except (json.JSONDecodeError, TypeError) as e:
+            self.error = e
+            return
+        try:
+            data = dicttoxml.dicttoxml(data)
+        except Exception as e:
+            self.error = e
+        return data
