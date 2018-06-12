fe68e0330cc5034eb75c6e3b5981554115326d1b
takeshix <takeshix@adversec.com>
2018-06-12 15:27:26 +0200

    Add GUI support for x509cloner
    x509cloner is now available in the GUI. Calling it will open a dialog
    that allows to configure either selfsigning or signing with an existing
    CA cert and key.

--- deen/plugins/misc/x509.py
+++ deen/plugins/misc/x509.py
@@ -9,6 +9,9 @@ try:
 except ImportError:
     OPENSSL = False
 
+from PyQt5.QtWidgets import QFileDialog, QDialog
+from PyQt5 import QtCore, QtGui, QtWidgets
+
 from deen.exceptions import *
 from .. import DeenPlugin
 
@@ -100,10 +103,14 @@ class DeenPluginX509CertificateCloner(DeenPlugin):
     cmd_name = 'x509certificatecloner'
     cmd_help = ('Clone X.509 certificates with a'
                 'randomly generated RSA public key')
-    cmd_only = True
 
     def __init__(self):
         super(DeenPluginX509CertificateCloner, self).__init__()
+        self.parent = None
+        self.x509gui = None
+        self.file_open_dialog = None
+        self.ca_cert = None
+        self.ca_key = None
 
     @staticmethod
     def prerequisites():
@@ -149,40 +156,66 @@ class DeenPluginX509CertificateCloner(DeenPlugin):
         if (args.CA_CERT and not args.CA_KEY) or (not args.CA_CERT and args.CA_KEY):
             LOGGER.error('CA_CERT and CA_KEY required')
             sys.exit(1)
-        with open(args.CERT_TO_CLONE, 'rb') as f:
-            original_cert = f.read()
-        try:
-            original_cert = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, original_cert)
-        except OpenSSL.crypto.Error:
-            LOGGER.error('Invalid certificate '+ args.CERT_TO_CLONE)
-            sys.exit(1)
+        original_cert = self._load_cert(args.CERT_TO_CLONE)
         if args.signature_algorithm:
             signature_algo = args.signature_algorithm
         else:
             signature_algo = original_cert.get_signature_algorithm().decode()
-        new_cert, new_key = self._clone(original_cert, args.self_signed, args.out, signature_algo)
+        new_cert, new_key = self._clone(original_cert, args.self_signed, signature_algo)
         if args.CA_CERT:
-            with open(args.CA_CERT, 'rb') as f:
-                ca_cert = f.read()
-            try:
-                ca_cert = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, ca_cert)
-            except OpenSSL.crypto.Error as e:
-                LOGGER.error('Invalid certificate ' + args.CA_CERT)
-                sys.exit(1)
+            ca_cert = self._load_cert(args.CA_CERT)
             new_cert.set_issuer(ca_cert.get_issuer())
-            with open(args.CA_KEY, 'rb') as f:
-                ca_pkey = f.read()
-            try:
-                ca_pkey = OpenSSL.crypto.load_privatekey(OpenSSL.crypto.FILETYPE_PEM, ca_pkey)
-            except OpenSSL.crypto.Error:
-                LOGGER.error('Invalid private key ' + args.CA_KEY)
-                sys.exit(1)
+            ca_pkey = self._load_private_key(args.CA_KEY)
             new_cert.sign(ca_pkey, signature_algo)
         self._save_to_file(new_cert, new_key, args.out)
         ret = 'Saved new certificate as {}.cert'.format(args.out)
         return ret.encode()
 
-    def _clone(self, original_cert, self_sign, out, signature_algo):
+    def process_gui(self, parent, content):
+        """In order to accept multiple input values, we have to create
+        an additional GUI element that allows to choose to either selfsign
+        certificates or use an existing CA cert and key for signing."""
+        self.parent = parent
+        self.x509gui = X509CloneGui(self.parent)
+        self.file_open_dialog = QFileDialog(self.x509gui)
+        self.x509gui.ui.x509clone_selfsign.toggled.connect(self.x509gui.toggle_selfsign)
+        self.x509gui.ui.x509clone_casign.toggled.connect(self.x509gui.toggle_casign)
+        self.x509gui.ui.x509clone_load_cacert_button.clicked.connect(self._load_cert_dialog)
+        self.x509gui.ui.x509clone_load_cakey_button.clicked.connect(self._load_private_key_dialog)
+        if self.x509gui.exec_() == 0:
+            # If the plugin GUI is cancelled, just
+            # return without doing anything.
+            return
+        # Decode content because pyOpenSSL failes with bytearray.
+        content = content.decode()
+        try:
+            original_cert = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, content)
+        except (OpenSSL.crypto.Error, TypeError) as e:
+            LOGGER.error('Invalid certificate')
+            return
+        signature_algo = original_cert.get_signature_algorithm().decode()
+        if self.x509gui.ui.x509clone_selfsign.isChecked():
+            new_cert, new_key = self._clone(original_cert, True, signature_algo)
+        elif self.x509gui.ui.x509clone_casign.isChecked():
+            if not self.ca_cert:
+                LOGGER.error('Invalid CA cert')
+                return
+            if not self.ca_key:
+                LOGGER.error('Invalid CA key')
+                return
+            new_cert, new_key = self._clone(original_cert, False, signature_algo)
+            new_cert.set_issuer(self.ca_cert.get_issuer())
+            new_cert.sign(self.ca_key, signature_algo)
+        else:
+            LOGGER.error('No action selected')
+            return
+        content = OpenSSL.crypto.dump_privatekey(OpenSSL.crypto.FILETYPE_PEM, new_key)
+        content += OpenSSL.crypto.dump_certificate(OpenSSL.crypto.FILETYPE_PEM, new_cert)
+        return content
+
+    def _clone(self, original_cert, self_sign, signature_algo):
+        """Create a 1:1 clone of original_cert. If self_sign is False,
+        the new_certificate will not yet be signed."""
         cert = OpenSSL.crypto.X509()
         cert.set_version(original_cert.get_version())
         cert.set_serial_number(original_cert.get_serial_number())
@@ -204,6 +237,48 @@ class DeenPluginX509CertificateCloner(DeenPlugin):
             cert.sign(pkey, signature_algo)
         return cert, pkey
 
+    def _load_cert_dialog(self):
+        cert_path = self.file_open_dialog.getOpenFileName(
+            self.file_open_dialog, 'Load CA cert from file')[0]
+        if not cert_path:
+            return
+        self.ca_cert = self._load_cert(cert_path)
+        if not self.ca_cert:
+            self.parent.show_error_msg('Invalid CA cert ' + cert_path, parent=self.x509gui)
+
+    def _load_cert(self, filepath):
+        """Read the content of filepath and
+        return it an OpenSSL cert object."""
+        with open(filepath, 'rb') as f:
+            cert = f.read()
+        try:
+            cert = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, cert)
+        except OpenSSL.crypto.Error as e:
+            LOGGER.error('Invalid certificate ' + filepath)
+            return
+        return cert
+
+    def _load_private_key_dialog(self):
+        key_path = self.file_open_dialog.getOpenFileName(
+            self.file_open_dialog, 'Load CA private key from file')[0]
+        if not key_path:
+            return
+        self.ca_key = self._load_private_key(key_path)
+        if not self.ca_key:
+            self.parent.show_error_msg('Invalid private key ' + key_path, parent=self.x509gui)
+
+    def _load_private_key(self, filepath):
+        """Read the content of filepath and
+        return it an OpenSSL private key object."""
+        with open(filepath, 'rb') as f:
+            key = f.read()
+        try:
+            key = OpenSSL.crypto.load_privatekey(OpenSSL.crypto.FILETYPE_PEM, key)
+        except OpenSSL.crypto.Error:
+            LOGGER.error('Invalid private key ' + filepath)
+            return
+        return key
+
     def _save_to_file(self, cert, pkey, out_file):
         priv_pem = OpenSSL.crypto.dump_privatekey(OpenSSL.crypto.FILETYPE_PEM, pkey)
         cert_pem = OpenSSL.crypto.dump_certificate(OpenSSL.crypto.FILETYPE_PEM, cert)
@@ -214,3 +289,80 @@ class DeenPluginX509CertificateCloner(DeenPlugin):
         with open(out_file + '.pem', 'wb') as f:
             f.write(priv_pem)
             f.write(cert_pem)
+
+
+class X509CloneGui(QDialog):
+    def __init__(self, parent):
+        super(X509CloneGui, self).__init__(parent)
+        self.ui = Ui_X509CloneGui()
+        self.ui.setupUi(self)
+        self.parent = parent
+        self.ui.x509clone_selfsign.setChecked(True)
+        self.ui.x509clone_selfsign_groupbox.setEnabled(True)
+        self.ui.x509clone_casign_groupbox.setEnabled(False)
+
+    def toggle_selfsign(self):
+        self.ui.x509clone_selfsign_groupbox.setEnabled(True)
+        self.ui.x509clone_casign_groupbox.setEnabled(False)
+
+    def toggle_casign(self):
+        self.ui.x509clone_selfsign_groupbox.setEnabled(False)
+        self.ui.x509clone_casign_groupbox.setEnabled(True)
+
+
+class Ui_X509CloneGui(object):
+    def setupUi(self, X509CloneGui):
+        X509CloneGui.setObjectName("X509CloneGui")
+        X509CloneGui.resize(597, 398)
+        self.gridLayout = QtWidgets.QGridLayout(X509CloneGui)
+        self.gridLayout.setObjectName("gridLayout")
+        self.x509clone_casign_groupbox = QtWidgets.QGroupBox(X509CloneGui)
+        self.x509clone_casign_groupbox.setTitle("")
+        self.x509clone_casign_groupbox.setObjectName("x509clone_casign_groupbox")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.x509clone_casign_groupbox)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.plainTextEdit_2 = QtWidgets.QPlainTextEdit(self.x509clone_casign_groupbox)
+        self.plainTextEdit_2.setObjectName("plainTextEdit_2")
+        self.verticalLayout.addWidget(self.plainTextEdit_2)
+        self.x509clone_load_cacert_button = QtWidgets.QPushButton(self.x509clone_casign_groupbox)
+        self.x509clone_load_cacert_button.setObjectName("x509clone_load_cacert_button")
+        self.verticalLayout.addWidget(self.x509clone_load_cacert_button)
+        self.x509clone_load_cakey_button = QtWidgets.QPushButton(self.x509clone_casign_groupbox)
+        self.x509clone_load_cakey_button.setObjectName("x509clone_load_cakey_button")
+        self.verticalLayout.addWidget(self.x509clone_load_cakey_button)
+        self.gridLayout.addWidget(self.x509clone_casign_groupbox, 1, 3, 1, 1)
+        self.x509clone_ok_button = QtWidgets.QDialogButtonBox(X509CloneGui)
+        self.x509clone_ok_button.setOrientation(QtCore.Qt.Horizontal)
+        self.x509clone_ok_button.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Ok)
+        self.x509clone_ok_button.setObjectName("x509clone_ok_button")
+        self.gridLayout.addWidget(self.x509clone_ok_button, 4, 3, 1, 1)
+        self.x509clone_selfsign_groupbox = QtWidgets.QGroupBox(X509CloneGui)
+        self.x509clone_selfsign_groupbox.setTitle("")
+        self.x509clone_selfsign_groupbox.setObjectName("x509clone_selfsign_groupbox")
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.x509clone_selfsign_groupbox)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.plainTextEdit = QtWidgets.QPlainTextEdit(self.x509clone_selfsign_groupbox)
+        self.plainTextEdit.setObjectName("plainTextEdit")
+        self.verticalLayout_2.addWidget(self.plainTextEdit)
+        self.gridLayout.addWidget(self.x509clone_selfsign_groupbox, 1, 2, 1, 1)
+        self.x509clone_casign = QtWidgets.QRadioButton(X509CloneGui)
+        self.x509clone_casign.setObjectName("x509clone_casign")
+        self.gridLayout.addWidget(self.x509clone_casign, 0, 3, 1, 1)
+        self.x509clone_selfsign = QtWidgets.QRadioButton(X509CloneGui)
+        self.x509clone_selfsign.setObjectName("x509clone_selfsign")
+        self.gridLayout.addWidget(self.x509clone_selfsign, 0, 2, 1, 1)
+
+        self.retranslateUi(X509CloneGui)
+        self.x509clone_ok_button.accepted.connect(X509CloneGui.accept)
+        self.x509clone_ok_button.rejected.connect(X509CloneGui.reject)
+        QtCore.QMetaObject.connectSlotsByName(X509CloneGui)
+
+    def retranslateUi(self, X509CloneGui):
+        _translate = QtCore.QCoreApplication.translate
+        X509CloneGui.setWindowTitle(_translate("X509CloneGui", "Dialog"))
+        self.plainTextEdit_2.setPlainText(_translate("X509CloneGui", "Sign the new certificate with an existing CA certificate. The private key of the existing CA must be available."))
+        self.x509clone_load_cacert_button.setText(_translate("X509CloneGui", "Load CA certificate"))
+        self.x509clone_load_cakey_button.setText(_translate("X509CloneGui", "Load CA private key"))
+        self.plainTextEdit.setPlainText(_translate("X509CloneGui", "The new certificate will be self signed."))
+        self.x509clone_casign.setText(_translate("X509CloneGui", "Sign wi&th existing CA"))
+        self.x509clone_selfsign.setText(_translate("X509CloneGui", "Self si&gned"))
