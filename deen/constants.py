58ad7911691a5d6f72ed07e4e00a9950ee9f7ccd
takeshix <takeshix@adversec.com>
2018-07-05 10:11:23 +0200

    Bumped to v1.3.0

--- deen/constants.py
+++ deen/constants.py
@@ -1,3 +1,3 @@
-__version__ = '1.2.1'
+__version__ = '1.3.0'
 about_text = ('deen (DEcoderENcoder) v%s\n\nA decoding/encoding application for arbitrary data.\n\n'
               'https://github.com/takeshixx/deen') % __version__
