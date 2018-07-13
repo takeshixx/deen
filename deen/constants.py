4eb5adf2834f1931b4152ae4116eba8d5b555993
takeshix <takeshix@adversec.com>
2018-07-13 10:01:49 +0200

    Bumped to v1.4.5
    * Make error for empty plugin returns a debug message

--- deen/constants.py
+++ deen/constants.py
@@ -1,3 +1,3 @@
-__version__ = '1.4.4'
+__version__ = '1.4.5'
 about_text = ('deen (DEcoderENcoder) v%s\n\nA decoding/encoding application for arbitrary data.\n\n'
               'https://github.com/takeshixx/deen') % __version__
