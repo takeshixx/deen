f09ad5e81cfef3196f2af037785d0acefbeb9690
takeshix <takeshix@adversec.com>
2018-07-14 19:11:46 +0200

    Subclass DeenPluginAsmAarch64 from DeenPluginAsmArm

--- deen/plugins/assemblies/aarch64.py
+++ deen/plugins/assemblies/aarch64.py
@@ -6,9 +6,10 @@ except ImportError:
     KEYSTONE = False
 
 from .__base__ import AsmBase
+from .arm import DeenPluginAsmArm
 
 
-class DeenPluginAsmAarch64(AsmBase):
+class DeenPluginAsmAarch64(DeenPluginAsmArm):
     name = 'assembly_aarch64'
     display_name = 'AARCH64'
     aliases = ['asm_aarch64',
@@ -17,12 +18,6 @@ class DeenPluginAsmAarch64(AsmBase):
     cmd_name = 'assembly_aarch64'
     cmd_help='Assemble/Disassemble for the AARCH64 architecture'
     keystone_arch = keystone.KS_ARCH_ARM64 if KEYSTONE else None
+    keystone_mode = 0 # There is only the default mode for AARCH64
     capstone_arch = capstone.CS_ARCH_ARM64 if KEYSTONE else None
-
-    def __init__(self):
-        super(DeenPluginAsmAarch64, self).__init__()
-        # Initialize keystone and capstone as soon as an instance
-        # of this plugin will be created.
-        if KEYSTONE:
-            self.ks = keystone.Ks(self.keystone_arch, keystone.KS_MODE_LITTLE_ENDIAN)
-            self.cs = capstone.Cs(self.capstone_arch, keystone.KS_MODE_LITTLE_ENDIAN)
+    capstone_mode = 0 # There is only the default mode for AARCH64
