36fe68123bf4c863c602bc96dbbbad3e8c548f23
takeshix <takeshix@adversec.com>
2018-07-25 14:08:07 +0200

    Added endianess.py plugin

---
+++ deen/plugins/misc/endianness.py
@@ -0,0 +1,17 @@
+from .. import DeenPlugin
+
+
+class DeenPluginEndianess(DeenPlugin):
+    name = 'endianness'
+    display_name = 'Swap Endianness'
+    aliases = ['endian']
+    cmd_name = 'endianness'
+    cmd_help='Swap byte order between little- and big-endian.'
+
+    def __init__(self):
+        super(DeenPluginEndianess, self).__init__()
+
+    def process(self, data):
+        super(DeenPluginEndianess, self).process(data)
+        data.reverse()
+        return data
