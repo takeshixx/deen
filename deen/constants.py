bc45c074c44462529b9036f06c1f7fde9a361918
takeshix <takeshix@adversec.com>
2019-02-06 14:12:01 +0100

    Bumped to v1.7.1
    * Notify invalid plugins in statusbar.

--- deen/constants.py
+++ deen/constants.py
@@ -1,4 +1,4 @@
-__version__ = '1.7.0'
+__version__ = '1.7.1'
 
 about_text = ('deen (DEcoderENcoder) v%s\n\nA decoding/encoding application for arbitrary data.\n\n'
               'https://github.com/takeshixx/deen') % __version__
