b1697c2c0af91ae19e7872d7472780904c8815e1
takeshix <takeshix@adversec.com>
2019-05-16 22:55:11 +0200

    Bumped to v2.0.0
    * New GUI layout
    * Live editiing reactivated
    * Formatted view for plugins like syntax highlight
    * Many improvements for the hex view
    * Editing the content of one widget will affect the content of all
    following widgets
    * Added various key shortcuts (see repo wiki)
    * Improved logging facilities
    * Added --fullscreen CLI option
    * Convenience wrappers for unprocess() added (.-prefix on CLI, .- and
    --prefix in GUI fuzzy search)
    * Many bug fixes

--- deen/constants.py
+++ deen/constants.py
@@ -1,4 +1,4 @@
-__version__ = '2.0.0b5'
+__version__ = '2.0.0'
 
 about_text = ('deen (DEcoderENcoder) v%s\n\nA decoding/encoding application for arbitrary data.\n\n'
               'https://github.com/takeshixx/deen') % __version__
